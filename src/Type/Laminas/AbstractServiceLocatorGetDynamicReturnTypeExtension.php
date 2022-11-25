f3fa53fb96de289f50de64cd0e929ce7bbcf1897
InvisibleSmiley <github@hatlak.de>
2022-11-25 16:28:36 +0100

    Support container interfaces for get() return type (#39)
    Co-authored-by: Jens Hatlak <jens.hatlak@check24.de>

---
+++ src/Type/Laminas/AbstractServiceLocatorGetDynamicReturnTypeExtension.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasPhpStan\Type\Laminas;
+
+use Laminas\ServiceManager\AbstractPluginManager;
+use LaminasPhpStan\ServiceManagerLoader;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\NeverType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use ReflectionClass;
+
+abstract class AbstractServiceLocatorGetDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+    public function __construct(
+        private ReflectionProvider $reflectionProvider,
+        private ServiceManagerLoader $serviceManagerLoader
+    ) {
+    }
+
+    final public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return 'get' === $methodReflection->getName();
+    }
+
+    final public function getTypeFromMethodCall(
+        MethodReflection $methodReflection,
+        MethodCall $methodCall,
+        Scope $scope
+    ): Type {
+        $calledOnType = $scope->getType($methodCall->var);
+        if (! $calledOnType instanceof ObjectType) {
+            return new MixedType();
+        }
+
+        $args = $methodCall->getArgs();
+        if (1 !== \count($args)) {
+            return new MixedType();
+        }
+
+        $serviceManager = $this->serviceManagerLoader->getServiceLocator($calledOnType->getClassName());
+
+        $firstArg = $args[0];
+        if (! $firstArg instanceof Arg) {
+            throw new \PHPStan\ShouldNotHappenException(\sprintf(
+                'Argument passed to %s::%s should be a string, %s given',
+                $methodReflection->getDeclaringClass()->getName(),
+                $methodReflection->getName(),
+                $firstArg->getType()
+            ));
+        }
+        $argType = $scope->getType($firstArg->value);
+        if (! $argType instanceof ConstantStringType) {
+            if ($serviceManager instanceof AbstractPluginManager) {
+                $refClass    = new ReflectionClass($serviceManager);
+                $refProperty = $refClass->getProperty('instanceOf');
+                $refProperty->setAccessible(true);
+                $returnedInstance = $refProperty->getValue($serviceManager);
+                \assert(null === $returnedInstance || \is_string($returnedInstance));
+                if (null !== $returnedInstance && $this->reflectionProvider->hasClass($returnedInstance)) {
+                    return new ObjectType($returnedInstance);
+                }
+            }
+
+            return new MixedType();
+        }
+
+        $serviceName = $argType->getValue();
+        if (! $serviceManager->has($serviceName)) {
+            return new NeverType();
+        }
+
+        if (\class_exists($serviceName) || \interface_exists($serviceName)) {
+            return new ObjectServiceManagerType($serviceName, $serviceName);
+        }
+
+        $service = $serviceManager->get($serviceName);
+        if (\is_object($service)) {
+            $className = $service::class;
+            $refClass  = new ReflectionClass($service);
+            if ($refClass->isAnonymous()) {
+                if (false !== ($parentClass = $refClass->getParentClass())) {
+                    $className = $parentClass->getName();
+                } elseif ([] !== ($interfaces = $refClass->getInterfaces())) {
+                    $className = \current($interfaces)->getName();
+                }
+            }
+
+            return new ObjectServiceManagerType($className, $serviceName);
+        }
+
+        return $scope->getTypeFromValue($service);
+    }
+}
---
+++ src/Type/Laminas/InteropContainerGetDynamicReturnTypeExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasPhpStan\Type\Laminas;
+
+use Interop\Container\Containerinterface;
+
+final class InteropContainerGetDynamicReturnTypeExtension extends AbstractServiceLocatorGetDynamicReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return ContainerInterface::class;
+    }
+}
